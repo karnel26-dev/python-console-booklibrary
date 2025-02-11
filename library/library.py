ce62ec33d5d4bf8defe721448c9854a46b8501c5
karnel26-dev <karnel26@gmail.com>
2025-02-11 18:21:08 +0300

    add search_book methods

--- library/library.py
+++ library/library.py
@@ -12,19 +12,38 @@ class Library:
             Library.id_ += 1
             self.books[Library.id_] = book
             self.storage.write_data(book.to_dict())
+            return book
+        raise ValueError("Неверный формат книги!")
 
-    def get_book_info(self, book_id):
-        return self.books.get(book_id)
+    def get_book_by_id(self, book_id):
+        book = self.books.get(book_id)
+        if book:
+            return book
+        raise ValueError("Такой книги нет")
+
+    def get_book_by_isbn(self, isbn):
+        for id_, book in self.books.items():
+            if isbn == book.get("isbn"):
+                return id_, book
+
+        raise ValueError("Такой книги нет")
 
     def get_books(self):
         return self.books
 
-    def search_book(self, query):
-        results = {}
+    def get_books_by_author(self, author):
+        books = {}
         for id_, book in self.books.items():
-            if query.lower() in book.author.lower():
-                results[id_] = book
-        return results
+            if author.lower() in book.author.lower():
+                books[id_] = book
+        return books
+
+    def get_books_by_title(self, title):
+        books = {}
+        for id_, book in self.books.items():
+            if title.lower() in book.title.lower():
+                books[id_] = book
+        return books
 
     def book_delete(self, id_):
         if id_.isdigit():
